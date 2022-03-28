c9a8284c275e760e04c0ca75dd4e234f7f5c4c0c
VasiliyBEL <vaspn001@gmail.com>
2022-03-28 16:06:42 +0300

    Add project files.

---
+++ ScreenShotAppWindows/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ScreenShotAppWindows
+{
+    public partial class ScreenShot : Form
+    {
+        public ScreenShot()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Thread.Sleep(1000);
+            SendKeys.Send("{PRTSC}");
+            Image myImage = Clipboard.GetImage();
+            pictureBox1.Image = myImage;
+            myImage.Save(@"C:\Users\Public\Pictures\image.jpg");
+            this.Show();
+        }
+    }
+}
